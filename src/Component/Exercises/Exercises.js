5a3c61cc0dfc6e47351c8073b64866fd0ca54d8d
Shafayet-Hossain23 <mdshafayethossain6530@gmail.com>
2022-09-29 21:51:24 +0600

    question-section

--- src/Component/Exercises/Exercises.js
+++ src/Component/Exercises/Exercises.js
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react';
 import './Exercises.css'
 import Exercise from '../Exercise/Exercise';
 import User from '../User/User';
+import Question from '../Bonus-section/question';
 
 const Exercises = () => {
     const [exercises, setExercises] = useState([]);
@@ -18,30 +19,36 @@ const Exercises = () => {
         setSelected(newSelected)
     }
     return (
-        <div className='container'>
-            <div className='row'>
-                <div className='col-8'>
-                    <div className='exercises container'>
-                        <h4 className='default-color'>
-                            <FontAwesomeIcon icon={faWalking}></FontAwesomeIcon>
-                            <span className='fw-bolder ms-3 '>STRENGTH CENTER</span>
-                        </h4>
-                        <h5 className='mt-4 fw-semibold mb-4'>Select today's exercise</h5>
-                        <div className='exercises-container'>
-                            {
-                                exercises.map((exercise) => <Exercise exercise={exercise}
-                                    addToListHandler={addToListHandler}
-                                ></Exercise>)
-                            }
+        <div>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-8'>
+                        <div className='exercises container'>
+                            <h4 className='default-color'>
+                                <FontAwesomeIcon icon={faWalking}></FontAwesomeIcon>
+                                <span className='fw-bolder ms-3 '>STRENGTH CENTER</span>
+                            </h4>
+                            <h5 className='mt-4 fw-semibold mb-4'>Select today's exercise</h5>
+                            <div className='exercises-container'>
+                                {
+                                    exercises.map((exercise) => <Exercise exercise={exercise}
+                                        addToListHandler={addToListHandler}
+                                    ></Exercise>)
+                                }
+                            </div>
                         </div>
+
+
                     </div>
-
-
-                </div>
-                <div className='col-4'>
-                    <User selected={selected}></User>
+                    <div className='col-4'>
+                        <User selected={selected}></User>
+                    </div>
                 </div>
             </div>
+            <div className='container mt-5'>
+                <Question></Question>
+            </div>
+
         </div>
 
 
