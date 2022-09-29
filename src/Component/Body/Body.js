415f983b23ec1bfc300e597f0344c028dd0eafde
Shafayet-Hossain23 <mdshafayethossain6530@gmail.com>
2022-09-29 12:57:26 +0600

    Creating-component

---
+++ src/Component/Body/Body.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Exercises from '../Exercises/Exercises';
+import User from '../Exercises/User/User';
+
+const Body = () => {
+    return (
+        <div>
+            <Exercises></Exercises>
+            <User></User>
+        </div>
+    );
+};
+
+export default Body;
