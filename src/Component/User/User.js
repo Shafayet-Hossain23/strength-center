3736c579c8b238b793e17768d1c5c6e9089a00ef
Shafayet-Hossain23 <mdshafayethossain6530@gmail.com>
2022-09-29 16:26:15 +0600

    user-name-&-image

--- src/Component/User/User.js
+++ src/Component/User/User.js
@@ -1,9 +1,19 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-
+import { faCircleUser, faLocationDot, faUser } from '@fortawesome/free-solid-svg-icons'
+import './User.css'
 const User = () => {
     return (
-        <div>
-            <h1>This is user part</h1>
+        <div className='user'>
+            <p>
+                <FontAwesomeIcon icon={faCircleUser}></FontAwesomeIcon> <br />
+                <span>Md. Junayed Hossain</span>
+            </p>
+            <p>
+                <FontAwesomeIcon icon={faLocationDot}></FontAwesomeIcon>
+                <span className='ms-2'>Mirzapur,Bangladesh</span>
+            </p>
+
         </div>
     );
 };
