2e530563b5ea977ece5cc134ea9bcea62dd25e42
Shafayet-Hossain23 <mdshafayethossain6530@gmail.com>
2022-09-29 17:14:17 +0600

    add-break

--- src/Component/User/User.js
+++ src/Component/User/User.js
@@ -4,15 +4,55 @@ import { faCircleUser, faLocationDot, faUser } from '@fortawesome/free-solid-svg
 import './User.css'
 const User = () => {
     return (
-        <div className='user'>
-            <p>
-                <FontAwesomeIcon icon={faCircleUser}></FontAwesomeIcon> <br />
-                <span>Md. Junayed Hossain</span>
-            </p>
-            <p>
-                <FontAwesomeIcon icon={faLocationDot}></FontAwesomeIcon>
-                <span className='ms-2'>Mirzapur,Bangladesh</span>
-            </p>
+        <div className='user' >
+            <div >
+                <p>
+                    <FontAwesomeIcon className='userImage pt-4' icon={faCircleUser}></FontAwesomeIcon> <br />
+                    <span>Md. Junayed Hossain</span>
+                </p>
+                <p>
+                    <FontAwesomeIcon icon={faLocationDot}></FontAwesomeIcon>
+                    <span className='ms-2'>Mirzapur,Bangladesh</span>
+                </p>
+            </div>
+            <div className='user-info'>
+                <div>
+                    <p><span className='fw-bold'>75 </span>kg</p>
+                    <p>Weight</p>
+
+                </div>
+                <div>
+                    <p><span className='fw-bold'>6.5 </span>inch</p>
+                    <p>Height</p>
+
+                </div>
+                <div>
+                    <p><span className='fw-bold'>20 </span>yrs</p>
+                    <p>Age</p>
+
+                </div>
+
+
+            </div>
+            <div className='mt-5'>
+                <p className='fw-bold'>Add a Break</p>
+                <div className='break'>
+                    <div className='hover1' >
+                        <p className='rounded-circle bg-white p-2 m-2'>5m</p>
+                    </div>
+                    <div className='hover1'>
+                        <p className='rounded-circle bg-white p-2 m-2'>10m</p>
+                    </div>
+                    <div className='hover1'>
+                        <p className='rounded-circle bg-white p-2 m-2'>15m</p>
+                    </div>
+                    <div className='hover1'>
+                        <p className='rounded-circle bg-white p-2 m-2'>20m</p>
+                    </div>
+
+
+                </div>
+            </div>
 
         </div>
     );
